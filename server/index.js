6cda2d571df4b11e1b5ff4f6836c27e48332f9f9
Johan Perez <1johanperez@gmail.com>
2021-10-02 01:05:32 -0400

    commit after proj set up

--- server/index.js
+++ server/index.js
@@ -1,32 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-
 const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}!!! WHOHOO!`);
+});
 
-const { dbConnection } = require("./db");
-
-const startServer = async () => {
-    await dbConnection.sync();
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}!~`);
-    });
-};
-startServer();
-
-// Matches any url for a GET request to a possible file
-// in the public directory.
-app.use(express.static(__dirname + "/public"));
-
-// Start of all middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-const genresRouter = require("./routes/genre");
-app.use("/genre", genresRouter);
-
-const moviesRouter = require("./routes/movie");
-app.use("/movies", moviesRouter);
-
-app.get("/", (req, res) => {
-    res.send("Hello :)");
+app.get('/', (req, res) => {
+  res.send(`Hello World!`);
 });
